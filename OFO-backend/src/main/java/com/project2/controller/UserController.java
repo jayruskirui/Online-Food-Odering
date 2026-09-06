fd7ec2ae4174c1bfd49ffdf51c0aaa91270a49a1
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-09-06 15:54:23 +0300

    creating of userservice and usercontroller

---
+++ OFO-backend/src/main/java/com/project2/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.project2.controller;
+
+import com.project2.service.UserService;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> findUserByJwtToken(@RequestHeader("Athorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+}
---
+++ OFO-backend/src/main/java/com/project2/service/UserServiceImp.java
@@ -0,0 +1,37 @@
+package com.project2.service;
+
+import com.project2.config.JwtProvider;
+import com.project2.repository.UserRepository;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserServiceImp implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Override
+    public User findUserByJwtToken(String jwt) throws Exception {
+        String email=jwtProvider.getEmailFromJwtToken(jwt);
+        User user=findUserByEmail(email);
+        return user;
+    }
+
+    @Override
+    public User findUserByEmail(String email) throws Exception {
+
+        User user = userRepository.findByEmail(email);
+
+        if(user==null){
+            throw new Exception("user not found");
+        }
+
+        return user;
+    }
+}
