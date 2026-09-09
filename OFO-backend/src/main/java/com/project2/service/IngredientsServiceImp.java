dee825cb14888ac3d9fe449c134661c7f7d5f05d
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-09-09 16:31:45 +0300

    creating the ingredients service and its implements and repositories

---
+++ OFO-backend/src/main/java/com/project2/service/IngredientsServiceImp.java
@@ -0,0 +1,92 @@
+package com.project2.service;
+
+import com.project2.repository.IngredientCategoryRepository;
+import com.project2.repository.IngredientItemRepository;
+import model.IngredientCategory;
+import model.IngredientsItem;
+import model.Restaurant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class IngredientsServiceImp implements IngredientsService{
+
+    @Autowired
+    private IngredientCategoryRepository ingredientCategoryRepository;
+
+    @Autowired
+    private IngredientItemRepository ingredientItemRepository;
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+
+    @Override
+    public IngredientCategory createIngredientCategory(String name, Long restaurantId) throws Exception {
+
+        Restaurant restaurant = restaurantService.findRestaurantById(restaurantId);
+
+        IngredientCategory category = new IngredientCategory();
+        category.setRestaurant(restaurant);
+        category.setName(name);
+
+        return ingredientCategoryRepository.save(category);
+    }
+
+    @Override
+    public IngredientCategory findIngredientCategoryById(Long id) throws Exception {
+
+        Optional<IngredientCategory> opt = ingredientCategoryRepository.findById(id);
+
+        if(opt.isEmpty()){
+            throw new Exception("Ingredient category not found");
+        }
+
+        return opt.get();
+    }
+
+    @Override
+    public List<IngredientCategory> findIngredientCategoryByRestaurantId(Long id) throws Exception {
+        restaurantService.findRestaurantById(id);
+        return ingredientCategoryRepository.findByRestaurantId(id);
+    }
+
+    @Override
+    public IngredientsItem createIngredientItem(Long restaurantId, String ingredientName, Long categoryId) throws Exception {
+
+        Restaurant restaurant = restaurantService.findRestaurantById(restaurantId);
+        IngredientCategory category=findIngredientCategoryById(categoryId);
+
+        IngredientsItem item = new IngredientsItem();
+        item.setName(ingredientName);
+        item.setRestaurant(restaurant);
+        item.setCategory(category);
+
+        IngredientsItem ingredient = ingredientItemRepository.save(item);
+        category.getIngredients().add(ingredient);
+
+        return ingredient;
+    }
+
+    @Override
+    public List<IngredientsItem> findRestaurantsIngredients(Long restaurantId) {
+        return ingredientItemRepository.findByRestaurantId(restaurantId);
+    }
+
+    @Override
+    public IngredientsItem updateStock(Long id) throws Exception {
+
+        Optional<IngredientsItem> optionalIngredientsItem = ingredientItemRepository.findById(id);
+
+        if(optionalIngredientsItem.isEmpty()){
+            throw new Exception("Ingredient not found");
+        }
+        IngredientsItem ingredientsItem = optionalIngredientsItem.get();
+        ingredientsItem.setInStoke(!ingredientsItem.isInStoke());
+
+        return ingredientItemRepository.save(ingredientsItem);
+    }
+}
