2a8da18a8568749c76ee9acd5d0442c6dee47102
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-09-12 23:03:49 +0300

    creating of theme and components

--- OFO-frontend/src/App.jsx
+++ OFO-frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import { darkTheme } from "./Theme/DarkTheme"
+import { ThemeProvider } from "@mui/material/styles"
+import Navbar from "./components/Navbar"
+import CssBaseline from "@mui/material/CssBaseline"
 
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <Navbar />
+      <CssBaseline />
+    </ThemeProvider>
   )
 }
 
