3f905131a330c2007a3eb8796e3216d942cfab53
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-09-13 18:12:24 +0300

    creating a navbar

--- OFO-frontend/src/components/Navbar.jsx
+++ OFO-frontend/src/components/Navbar.jsx
@@ -1,8 +1,43 @@
+import IconButton from '@mui/material/IconButton'
+import Avatar from '@mui/material/Avatar'
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className = 'px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
+        <div className = 'lg:mr-10 cursor-pointer flex items-center space-x-4'>
+          <div className = ' logo text-gray-300 font-semibold text-2xl'>
+            Online Eats
+          </div>
+        </div>
+
+        <div className = 'flex items-center space-x-2 lg:space-x-10'>
+
+          <div className= ''>
+            <IconButton>
+              <SearchIcon sx = {{fontSize: "1.5rem"}} />
+            </IconButton> 
+          </div>
+
+          <div className =''>
+            <Avatar sx = {{bgcolor: "white", color: "pink.A400"}}>O</Avatar>
+          </div>
+
+          <div className= ''>
+            <IconButton>
+              <Badge color="primary" badgeContent={3  }>
+                <ShoppingCartIcon sx = {{fontSize: "1.5rem"}} />
+              </Badge>
+            </IconButton> 
+          </div>
+
+        </div>
+        
+    </div>
   )
 }
 
