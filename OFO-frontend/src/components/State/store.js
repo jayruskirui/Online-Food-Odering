454072bcbc049b5d02052768df0fc4b98f6bf512
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-09-17 01:29:49 +0300

    creating user redux

---
+++ OFO-frontend/src/components/State/store.js
@@ -0,0 +1,10 @@
+import { authReducer } from "./Authentication/Reducer";
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux"
+import { thunk } from "redux-thunk";
+
+
+const rooteReducer=combineReducers({
+    auth: authReducer,
+})
+
+export const store = legacy_createStore(rooteReducer, applyMiddleware(thunk));
