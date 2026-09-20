a8af7a47570256bb83255894c30905c8e89bd5d5
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-09-21 00:47:54 +0300

    creating redux for the restaurant

--- OFO-frontend/src/components/State/store.js
+++ OFO-frontend/src/components/State/store.js
@@ -1,10 +1,12 @@
 import { authReducer } from "./Authentication/Reducer";
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux"
 import { thunk } from "redux-thunk";
+import restaurantReducer from "./Restaurant/Reducer";
 
 
 const rooteReducer=combineReducers({
     auth: authReducer,
+    restaurant: restaurantReducer
 })
 
 export const store = legacy_createStore(rooteReducer, applyMiddleware(thunk));
